3210a1268f1ff8908691ec0d658a2b8ee8bdd325
QUANG AN <quangan10032005@gmail.com>
2025-11-07 22:33:29 +0700

    init project

---
+++ saleapp/eapp/dao.py
@@ -0,0 +1,14 @@
+from eapp.models import Product, Category
+
+def load_categories():
+    return Category.query.all()
+def load_products(cate_id=None, kw=None, page=1):
+    query = Product.query
+    if kw:
+        query = query.filter(Product.name.contains(kw))
+
+    if cate_id:
+        query = query.filter(Product.category_id.__eq__(cate_id))
+
+    return query.all()
+
---
+++ saleapp/eapp/index.py
@@ -0,0 +1,19 @@
+from flask import render_template, request
+from eapp import app, dao
+
+
+@app.route('/')
+def index():
+    categories = dao.load_categories()
+
+
+    products = dao.load_products(cate_id=request.args.get('category_id'),
+                                 kw=request.args.get('kw'),
+                                 page=request.args.get('page'))
+
+    return render_template('index.html', categories=categories, products=products)
+
+
+if __name__ == '__main__':
+    from eapp import admin
+    app.run(debug=True)
